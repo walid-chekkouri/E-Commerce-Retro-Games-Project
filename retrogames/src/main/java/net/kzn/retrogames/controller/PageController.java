f02ea7d1f0fa434888c7e0860e542290b1c94c13
Moad <Moad@DESKTOP-EUUCC0U>
2020-10-29 16:55:34 +0100

    Part 02 - 04 - Understanding @RequestParam and @PathVariable annotations

--- retrogames/src/main/java/net/kzn/retrogames/controller/PageController.java
+++ retrogames/src/main/java/net/kzn/retrogames/controller/PageController.java
@@ -1,7 +1,9 @@
 package net.kzn.retrogames.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -16,5 +18,24 @@ public class PageController {
 		return mv;
 	}
 	
+
+//	@RequestMapping(value="/test")
+//	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
+//		if(greeting==null) {
+//			greeting = "Hello there";
+//		}
+//		ModelAndView mv = new ModelAndView("page");
+//		mv.addObject("greeting", greeting);
+//		return mv;
+//	}
 	
+	@RequestMapping(value="/test/{greeting}")
+	public ModelAndView test(@PathVariable("greeting")String greeting) {
+		if(greeting==null) {
+			greeting = "Hello there";
+		}
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("greeting", greeting);
+		return mv;
+	}
 }
