f53389113649d2bdcb28a72fdb5c39ab62109fea
Moad <Moad@DESKTOP-EUUCC0U>
2020-10-29 22:19:08 +0100

    Creating Master Page

--- retrogames/src/main/java/net/kzn/retrogames/controller/PageController.java
+++ retrogames/src/main/java/net/kzn/retrogames/controller/PageController.java
@@ -14,10 +14,30 @@ public class PageController {
 	{
 		
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring Web MVC!");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
+	@RequestMapping(value= {"/about"})
+	public ModelAndView about()
+	{
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping(value= {"/contact"})
+	public ModelAndView contact()
+	{
+		
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
+		return mv;
+	}
 
 //	@RequestMapping(value="/test")
 //	public ModelAndView test(@RequestParam(value="greeting", required=false)String greeting) {
--- retrogames/src/main/webapp/WEB-INF/views/shared/footer.jsp
+++ retrogames/src/main/webapp/WEB-INF/views/shared/footer.jsp
@@ -1,4 +1,4 @@
-    <div class="container footer">
+    <div class="container">
 
         <hr>
 
@@ -6,7 +6,7 @@
         <footer>
             <div class="row">
                 <div class="col-lg-12">
-                    <p>Copyright &copy; Online Shopping 2017 - Designed and Developed By Khozema Nullwala</p>
+                    <p>Copyright &copy; Your Website 2014</p>
                 </div>
             </div>
         </footer>
