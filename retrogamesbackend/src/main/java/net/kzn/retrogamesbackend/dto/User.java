a1ced90d25e4c468fc8093049d04e892ce053b13
Moad <Moad@DESKTOP-EUUCC0U>
2021-01-01 23:42:51 +0100

    Hibernate OneToOne Bi-Directional Mapping

--- retrogamesbackend/src/main/java/net/kzn/retrogamesbackend/dto/User.java
+++ retrogamesbackend/src/main/java/net/kzn/retrogamesbackend/dto/User.java
@@ -1,10 +1,12 @@
 package net.kzn.retrogamesbackend.dto;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -26,7 +28,14 @@ public class User{
 	private String password;
 	private boolean enabled = true;
 	
-	
+	@OneToOne(mappedBy = "user", cascade = CascadeType.ALL)
+	private Cart cart;
+	public Cart getCart() {
+		return cart;
+	}
+	public void setCart(Cart cart) {
+		this.cart = cart;
+	}
 	public int getId() {
 		return id;
 	}
